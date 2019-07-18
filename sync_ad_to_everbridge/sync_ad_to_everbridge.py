b4ece81d6b55eef17b7214b051ca885389008429
=Tyler Aratani <=tyler.aratani@hawaii.gov>
2019-07-18 09:53:09 -1000

    Moved all code to functions

--- sync_ad_to_everbridge/sync_ad_to_everbridge.py
+++ sync_ad_to_everbridge/sync_ad_to_everbridge.py
@@ -5,7 +5,7 @@ import json
 import base64
 import argparse
 from requests.exceptions import HTTPError
-def get_argparser():
+def get_Argparser():
     #Build argument parser.
     parser = argparse.ArgumentParser(description="Sync AD Group with Everbridge Group")
     #Get Filename or Location
@@ -21,16 +21,15 @@ def create_AuthHeader(username, password):
                                 'Content-Type': 'application/json',
                                 'return-client-request-id': 'true'}
     return header
-def get_token(id,secret,authority,url):
+def get_Token(id,secret,authority,url):
     context = adal.AuthenticationContext(authority)
     token = context.acquire_token_with_client_credentials(url,id,secret)
     return token
-
 def get_AzureGroups(auth,tenant,id,secret,url,inquiry,groupName,groupId):
     #Fetch Azure AD Group with Adal
     authority = auth + tenant
     #Request Token
-    token = get_token(id,secret,authority,url)
+    token = get_Token(id,secret,authority,url)
     #Create Rest Session
     SESSION = requests.Session()
     SESSION.headers.update({'Authorization': f"Bearer {token['accessToken']}",
@@ -138,29 +137,15 @@ def create_user(firstName,lastName,phone,email,url,header):
 def get_EverContacts(filterString, header, org):
     #Get a list of contacts from Everbridge
     return get_EverBridge('https://api.everbridge.net/rest/contacts/'+ org + '/?sortBy="lastName"&searchType=OR'+ filterString,header,None)
-def post_EverbridgeGroups(username,password,org,groupData,groupName):
-    print(len(groupData))
-    if len(groupData) < 1 or len(username) < 1 or len(password) < 1:
-        return None
-    #Convert username and password to base64
-    header = create_AuthHeader(username,password)
-    #Create the search query for the group Everbridge Contacts
+def create_query(filterArray,groupData,org,header):
     filterString = ""
-    filterArray = []
     for contact in groupData:
-        #Each Filter can only have 1 full name, so a contact filter must be created for each person
+    #Each Filter can only have 1 full name, so a contact filter must be created for each person
         filterData = create_filter(contact["givenName"],contact["surname"],'https://api.everbridge.net/rest/contactFilters/' + org +'/',header)
         filterString += "&contactFilterIds=" + str(filterData["id"])
         filterArray.append(filterData["id"])
-    
-    #Grabs the contacts from Everbridge with the given contact filters
-    everData = get_EverContacts(filterString, header, org)
-    contactList = []
-    contactCheck = []
-    groupBackup = []
-    #Delete Filters once they have been used
-    for filterId in filterArray:
-        delete_Everbridge('https://api.everbridge.net/rest/contactFilters/' + org + '/' + str(filterId),header,None) 
+    return filterString
+def create_EverContacts(contactList, contactCheck,groupData, groupBackup, everData, org, header):
     #Adds contact ID to Group Contact List
     for contact in everData["page"]["data"]:
         contactList.append(contact["id"])
@@ -171,11 +156,11 @@ def post_EverbridgeGroups(username,password,org,groupData,groupName):
             if check["name"] == contact["givenName"] + " " + contact["surname"]:
                 groupData.remove(contact)
                 groupBackup.append(contact)
-    
     #Inserts New User to Everbridge if GroupData is not empty
     for contact in groupData:
         newUser = create_user(contact["givenName"],contact["surname"],contact["businessPhones"],contact["mail"],'https://api.everbridge.net/rest/contacts/' + org + '/',header)
         contactList.append(newUser["id"])
+def delete_EverContacts(org,groupName,header,groupBackup):
     #Deletes extra users in group
     #Get all current users in group
     everGroup = get_EverBridge('https://api.everbridge.net/rest/contacts/groups/' + org + '?byType=name&groupName=' + groupName + '&pageSize=100&pageNumber=1',header,None)
@@ -192,12 +177,33 @@ def post_EverbridgeGroups(username,password,org,groupData,groupName):
             deleteList.append(contact["id"])
         if(len(deleteList) > 0):
             deleteRequests = delete_Everbridge('https://api.everbridge.net/rest/groups/' + org + '/contacts?byType=name&groupName=' + groupName + '&idType=id',header,deleteList)
+def add_contacts(org,groupName,header,contactList):
+    post_Everbridge('https://api.everbridge.net/rest/groups/' + org + '/contacts?byType=name&groupName=' + groupName + '&idType=id',header,contactList)   
+def sync_EverbridgeGroups(username,password,org,groupData,groupName):
+    if len(groupData) < 1 or len(username) < 10 or len(password) < 8 or len(org) < 10 or len(groupName) < 1:
+        return None
+    #Convert username and password to base64
+    header = create_AuthHeader(username,password)
+    #Create the search query for the group Everbridge Contacts
+    filterArray = []
+    filterString = create_query(filterArray, groupData,org,header)
+    #Grabs the contacts from Everbridge with the given contact filters
+    everData = get_EverContacts(filterString, header, org)
+    #Delete Filters once they have been used
+    for filterId in filterArray:
+        delete_Everbridge('https://api.everbridge.net/rest/contactFilters/' + org + '/' + str(filterId),header,None) 
+    contactList = []
+    contactCheck = []
+    groupBackup = []
+    #Create new contacts
+    create_EverContacts(contactList,contactCheck,groupData,groupBackup,everData,org,header)
+    #Delete extra users in group
+    delete_EverContacts(org,groupName,header,groupBackup)
     #Inserts users to group
-    print(len(contactList))
-    addContacts = post_Everbridge('https://api.everbridge.net/rest/groups/' + org + '/contacts?byType=name&groupName=' + groupName + '&idType=id',header,contactList)   
+    add_contacts(org,groupName,header,contactList)
 if __name__ == '__main__':
-    args = get_argparser().parse_args()
+    args = get_Argparser().parse_args()
     config = json.load(open(args.filename))
     data = get_AzureGroups("https://login.microsoftonline.com/",
         config["adTenant"],config["clientId"],config["clientSecret"], "https://graph.microsoft.com/","v1.0/groups/",config["adGroupName"],config["adGroupId"])
-    post_EverbridgeGroups(config["everbridgeUsername"],config["everbridgePassword"],config["everbridgeOrg"],data,config["everbridgeGroup"])
+    sync_EverbridgeGroups(config["everbridgeUsername"],config["everbridgePassword"],config["everbridgeOrg"],data,config["everbridgeGroup"])
