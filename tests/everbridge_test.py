ab7719a0b0bec3c413f9c73e6ee4a050cbfc5ef8
=Tyler Aratani <=tyler.aratani@hawaii.gov>
2019-07-18 15:41:31 -1000

    Added more tests for Everbridge

--- tests/everbridge_test.py
+++ tests/everbridge_test.py
@@ -32,13 +32,35 @@ def test_EverbridgeFilter():
 	resp = get_EverBridge('https://api.everbridge.net/rest/contacts/'+ config["everbridgeOrg"] + '/?sortBy="lastName"&searchType=OR&contactFilterIds=' + str(filterTest["id"]),header,None)
 	delete_Everbridge('https://api.everbridge.net/rest/contactFilters/' + config["everbridgeOrg"] + '/' + str(filterTest["id"]),header,None) 
 	assert(resp["page"]["totalCount"] == 2)
+#Expected: Will return a HTTP error due to invalid phone number for US
 def test_InvalidContact():
 	header = create_AuthHeader(config["everbridgeUsername"],config["everbridgePassword"])
 	with pytest.raises(HTTPError):
 		newUser = create_user("Invalid","PhoneTest",["8081234567"],"PythonTest@hawallig.gov",'https://api.everbridge.net/rest/contacts/' + config["everbridgeOrg"] + '/',header)
-def test_DeleteContact():
+#Expected: Will return a ok message as the new contact will be created then deleted
+def test_InsertAndDeleteContact():
 	header = create_AuthHeader(config["everbridgeUsername"],config["everbridgePassword"])
-	newUser = create_user("Invalid","PhoneTest",["8087278435"],"PythonTest@hawallig.gov",'https://api.everbridge.net/rest/contacts/' + config["everbridgeOrg"] + '/',header)
+	newUser = create_user("Invalid","PhoneTest",["8087278435"],"PythonTest@hawaii.gov",'https://api.everbridge.net/rest/contacts/' + config["everbridgeOrg"] + '/',header)
+	infoUser =  get_EverBridge('https://api.everbridge.net/rest/contacts/'+ config["everbridgeOrg"] + '/' + str(newUser["id"]),header,None)
+	assert(infoUser["result"]["firstName"] == "Invalid")
 	deleteRequests = delete_Everbridge('https://api.everbridge.net/rest/contacts/' + config["everbridgeOrg"] + '/' + str(newUser["id"]),header,None)
 	assert(deleteRequests["message"] == "OK")
-	
+#Expected: Will create a new user and add that user to the group 
+def test_AddAndRemoveFromGroup():
+	header = create_AuthHeader(config["everbridgeUsername"],config["everbridgePassword"])
+	newUser = create_user("TestTest","Automated",["8087278435"],"NewTest@hawaii.gov",'https://api.everbridge.net/rest/contacts/' + config["everbridgeOrg"] + '/',header)
+	everGroup = get_EverBridge('https://api.everbridge.net/rest/contacts/groups/' + config["everbridgeOrg"] + '?byType=name&groupName=' + config["everbridgeGroup"] + 
+	'&pageSize=100&pageNumber=1',header,None)
+	assert(everGroup["page"]["totalCount"] == 8)
+	add_contacts(config["everbridgeOrg"],config["everbridgeGroup"],header,[newUser["id"]])
+	everGroup = get_EverBridge('https://api.everbridge.net/rest/contacts/groups/' + config["everbridgeOrg"] + '?byType=name&groupName=' + config["everbridgeGroup"] + 
+	'&pageSize=100&pageNumber=1',header,None)
+	assert(everGroup["page"]["totalCount"] == 9)
+	delete_Everbridge('https://api.everbridge.net/rest/groups/' + config["everbridgeOrg"] + '/contacts?byType=name&groupName=' + config["everbridgeGroup"]
+	 + '&idType=id',header,[newUser["id"]])
+	everGroup = get_EverBridge('https://api.everbridge.net/rest/contacts/groups/' + config["everbridgeOrg"] + '?byType=name&groupName=' + config["everbridgeGroup"] + 
+	'&pageSize=100&pageNumber=1',header,None)
+	assert(everGroup["page"]["totalCount"] == 8)
+	deleteRequests = delete_Everbridge('https://api.everbridge.net/rest/contacts/' + config["everbridgeOrg"] + '/' + str(newUser["id"]),header,None)
+	assert(deleteRequests["message"] == "OK")
+	 
