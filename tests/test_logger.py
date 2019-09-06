94a4314d2d7b712b9341b33999c300ea6cb94569
Yoshiaki Iinuma <iinumayoshiaki@gmail.com>
2019-09-06 09:02:50 -1000

    Moved logging setup function to api/logger.py

---
+++ tests/test_logger.py
@@ -0,0 +1,117 @@
+"""
+Tests api/logger.py
+
+-- logger log level --
+    50 CRITICAL
+    40 ERROR
+    30 WARNING
+    20 INFO
+    10 DEBUG
+    0  NOTEST
+----------------------
+"""
+import sys
+from os.path import dirname, abspath
+from api.logger import setup_logger
+from tests.mock_helper import  LoggingMock
+
+def test_setup_logger_with_empty_filename():
+    """
+    Should pass STDOUT to logging.basicConfig
+    """
+    # Set up mock function
+    mock = LoggingMock()
+    mock.setup()
+    # Call setup_logger
+    setup_logger(None)
+    # Check if correct args are passed
+    mock.access('logging.basicConfig').assert_called_with(
+        stream=sys.stdout,
+        level=20,
+        format='%(asctime)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p')
+    # Reinstate mocked functions
+    mock.restore()
+
+def test_setup_logger_with_filename():
+    """
+    Should pass the path to the log file to logging.basicConfig
+    """
+    filename = 'xxxx.log'
+    path = dirname(dirname(abspath(__file__))) +'/logs/' + filename
+    # Set up mock function
+    mock = LoggingMock()
+    mock.setup()
+    # Call setup_logger
+    setup_logger(filename)
+    # Check if correct args are passed
+    mock.access('logging.basicConfig').assert_called_with(
+        filename=path,
+        level=20,
+        format='%(asctime)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p')
+    # Reinstate mocked functions
+    mock.restore()
+
+def test_setup_logger_with_string_level():
+    """
+    Should pass numeric log level to logging.basicConfig
+    """
+    level = 'warning'
+    filename = 'xxxx.log'
+    path = dirname(dirname(abspath(__file__))) +'/logs/' + filename
+    # Set up mock function
+    mock = LoggingMock()
+    mock.setup()
+    # Call setup_logger
+    setup_logger(filename, level)
+    # Check if correct args are passed
+    mock.access('logging.basicConfig').assert_called_with(
+        filename=path,
+        level=30,
+        format='%(asctime)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p')
+    # Reinstate mocked functions
+    mock.restore()
+
+def test_setup_logger_with_numeric_level():
+    """
+    Should pass numeric log level to logging.basicConfig
+    """
+    level = 40
+    filename = 'xxxx.log'
+    path = dirname(dirname(abspath(__file__))) +'/logs/' + filename
+    # Set up mock function
+    mock = LoggingMock()
+    mock.setup()
+    # Call setup_logger
+    setup_logger(filename, level)
+    # Check if correct args are passed
+    mock.access('logging.basicConfig').assert_called_with(
+        filename=path,
+        level=40,
+        format='%(asctime)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p')
+    # Reinstate mocked functions
+    mock.restore()
+
+def test_setup_logger_with_empty_level():
+    """
+    Should pass default numeric log level to logging.basicConfig
+    """
+    level = None
+    filename = 'xxxx.log'
+    path = dirname(dirname(abspath(__file__))) +'/logs/' + filename
+    # Set up mock function
+    mock = LoggingMock()
+    mock.setup()
+    # Call setup_logger
+    setup_logger(filename, level)
+    # Check if correct args are passed
+    mock.access('logging.basicConfig').assert_called_with(
+        filename=path,
+        level=20,
+        format='%(asctime)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p')
+    # Reinstate mocked functions
+    mock.restore()
