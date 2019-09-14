f9645c514539883cfb2752372e187e0bc245eb0a
Yoshiaki Iinuma <iinumayoshiaki@gmail.com>
2019-09-13 17:04:24 -1000

    Added tests/test_everbridge.py

---
+++ tests/test_everbridge.py
@@ -0,0 +1,16 @@
+"""
+Tests Everbridge API Functions
+"""
+import json
+from unittest.mock import MagicMock
+import pytest
+from api.everbridge import Everbridge
+from tests.mock_helper import AdalMock, RequestsMock
+# pylint: disable=unused-import
+import tests.log_helper
+
+def test_xxx():
+    """
+    Should
+    """
+    pass
