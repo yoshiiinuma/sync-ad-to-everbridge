95e351b9887fc9ff3ceb40c040c58d4bf4e5989f
Yoshiaki Iinuma <iinumayoshiaki@gmail.com>
2019-09-13 17:01:46 -1000

    Added ContactTracker

---
+++ api/contact_tracker.py
@@ -0,0 +1,102 @@
+"""
+Keeps track of contacts in the list according to the operation
+"""
+
+class ContactTracker:
+    """
+    Keeps track of contacts in the list according to the operation
+    """
+    INSERT_CONTACT = 'INSERT_CONTACT'
+    UPDATE_CONTACT = 'UPDATE_CONTACT'
+    REMOVE_MEMBER = 'REMOVE_MEMBER'
+
+    def __init__(self):
+        self.new_contacts = []
+        self.updated_contacts = []
+        self.obsolete_contacts = []
+        self.obsolete_members = []
+        self.new_members = []
+
+    def set_new_members(self, members):
+        """
+        Sets new members
+        """
+        self.new_members = members
+
+    def push(self, optype, contact):
+        """
+        Keeps track of contacts in the list according to the operation
+        """
+        if optype == ContactTracker.INSERT_CONTACT:
+            self.new_contacts.append(contact)
+        elif optype == ContactTracker.UPDATE_CONTACT:
+            self.updated_contacts.append(contact)
+        elif optype == ContactTracker.REMOVE_MEMBER:
+            self.obsolete_members.append(contact)
+        else:
+            raise Exception('CONTACT_TRACKER.PUSH: OPTYPE NOT SUPPORTED ' + optype)
+
+    def get_contacts(self, optype):
+        """
+        Returns the contact list specified by optype
+        """
+        if optype == ContactTracker.INSERT_CONTACT:
+            return self.new_contacts
+        if optype == ContactTracker.UPDATE_CONTACT:
+            return self.updated_contacts
+        if optype == ContactTracker.REMOVE_MEMBER:
+            return self.obsolete_members
+        raise Exception('CONTACT_TRACKER.GET: OPTYPE NOT SUPPORTED ' + optype)
+
+    def get_upsert_contacts(self):
+        """
+        Returns Contacts for upserting
+        """
+        return self.updated_contacts + self.new_contacts
+
+    def get_inserted_contact_external_ids(self, per=100):
+        """
+        Returns the list of externalIds from new_contacts
+        """
+        filters = []
+        ids = ""
+        cnt = 0
+        for contact in self.new_contacts:
+            cnt += 1
+            ids += '&externalIds=' + contact['externalId']
+            if cnt % per == 0:
+                filters.append(ids)
+                ids = ""
+        if ids:
+            filters.append(ids)
+        return filters
+
+    def get_remove_member_ids(self):
+        """
+        Returns the list of Contact IDs from obsolete_members
+        """
+        return [con['id'] for con in self.obsolete_members]
+
+    def get_delete_contact_ids(self):
+        """
+        Returns the IDs of contacts which do/will not belong to any groups
+        """
+        self.obsolete_contacts = []
+        for contact in self.obsolete_members:
+            # groups attribute contains ids of groups which the user belongs to
+            # we should delete the contact if the user belongs to only the current one
+            if 'groups' in contact:
+                if len(contact['groups']) == 1:
+                    self.obsolete_contacts.append(contact['id'])
+        return self.obsolete_contacts
+
+    def report(self):
+        """
+        Returns size of each list
+        """
+        return {
+            'inserted_contacts': len(self.new_contacts),
+            'updated_contacts': len(self.updated_contacts),
+            'deleted_contacts': len(self.obsolete_contacts),
+            'added_members': len(self.new_members),
+            'removed_members': len(self.obsolete_members)}
---
+++ tests/test_contact_tracker.py
@@ -0,0 +1,122 @@
+"""
+Test ContactTracker
+"""
+from api.contact_tracker import ContactTracker
+
+def create_tracker():
+    """
+    Creates ContactTracker instacne with data
+    """
+    tracker = ContactTracker()
+    tracker.push(ContactTracker.INSERT_CONTACT, {'externalId': 'aaa1@test.com'})
+    tracker.push(ContactTracker.INSERT_CONTACT, {'externalId': 'aaa2@test.com'})
+    tracker.push(ContactTracker.INSERT_CONTACT, {'externalId': 'aaa3@test.com'})
+    tracker.push(ContactTracker.UPDATE_CONTACT, {'id': 1, 'externalId': 'bbb1@test.com'})
+    tracker.push(ContactTracker.UPDATE_CONTACT, {'id': 2, 'externalId': 'bbb2@test.com'})
+    tracker.push(ContactTracker.UPDATE_CONTACT, {'id': 3, 'externalId': 'bbb3@test.com'})
+    tracker.push(ContactTracker.REMOVE_MEMBER,
+                 {'id': 4, 'externalId': 'ccc4@test.com', 'groups': [1, 2]})
+    tracker.push(ContactTracker.REMOVE_MEMBER,
+                 {'id': 5, 'externalId': 'ccc5@test.com', 'groups': [1]})
+    tracker.push(ContactTracker.REMOVE_MEMBER,
+                 {'id': 6, 'externalId': 'ccc6@test.com'})
+    return tracker
+
+def test_insert_contact():
+    """
+    Should return INSERT_CONTACT contacts
+    """
+    tracker = create_tracker()
+    contacts = tracker.get_contacts(ContactTracker.INSERT_CONTACT)
+    expected = [
+        {'externalId': 'aaa1@test.com'},
+        {'externalId': 'aaa2@test.com'},
+        {'externalId': 'aaa3@test.com'}]
+    assert contacts == expected
+
+def test_update_contact():
+    """
+    Should return UPDATE_CONTACT contacts
+    """
+    tracker = create_tracker()
+    contacts = tracker.get_contacts(ContactTracker.UPDATE_CONTACT)
+    expected = [
+        {'id': 1, 'externalId': 'bbb1@test.com'},
+        {'id': 2, 'externalId': 'bbb2@test.com'},
+        {'id': 3, 'externalId': 'bbb3@test.com'}]
+    assert contacts == expected
+
+def test_remove_member():
+    """
+    Should return REMOVE_MEMBER contacts
+    """
+    tracker = create_tracker()
+    contacts = tracker.get_contacts(ContactTracker.REMOVE_MEMBER)
+    expected = [
+        {'id': 4, 'externalId': 'ccc4@test.com', 'groups': [1, 2]},
+        {'id': 5, 'externalId': 'ccc5@test.com', 'groups': [1]},
+        {'id': 6, 'externalId': 'ccc6@test.com'}]
+    assert contacts == expected
+
+def test_get_upsert_contacts():
+    """
+    Should return contacts for upsert
+    """
+    tracker = create_tracker()
+    contacts = tracker.get_upsert_contacts()
+    expected = [
+        {'id': 1, 'externalId': 'bbb1@test.com'},
+        {'id': 2, 'externalId': 'bbb2@test.com'},
+        {'id': 3, 'externalId': 'bbb3@test.com'},
+        {'externalId': 'aaa1@test.com'},
+        {'externalId': 'aaa2@test.com'},
+        {'externalId': 'aaa3@test.com'}]
+    assert contacts == expected
+
+def test_get_inserted_contact_external_ids():
+    """
+    Should return ids of inserted contacts
+    """
+    tracker = create_tracker()
+    ids = tracker.get_inserted_contact_external_ids()
+    exp = [('&externalIds=aaa1@test.com' +
+            '&externalIds=aaa2@test.com' +
+            '&externalIds=aaa3@test.com')]
+    assert ids == exp
+    ids = tracker.get_inserted_contact_external_ids(1)
+    exp = ['&externalIds=aaa1@test.com',
+           '&externalIds=aaa2@test.com',
+           '&externalIds=aaa3@test.com']
+    assert ids == exp
+    ids = tracker.get_inserted_contact_external_ids(2)
+    exp = ['&externalIds=aaa1@test.com&externalIds=aaa2@test.com',
+           '&externalIds=aaa3@test.com']
+    assert ids == exp
+
+def test_get_remove_member_ids():
+    """
+    Should return contact ids for removing members
+    """
+    tracker = create_tracker()
+    ids = tracker.get_remove_member_ids()
+    exp = [4, 5, 6]
+    assert ids == exp
+
+def test_get_delete_contact_ids():
+    """
+    Should return ids of contacts not belonging to any groups
+    """
+    tracker = create_tracker()
+    ids = tracker.get_delete_contact_ids()
+    exp = [5]
+    assert ids == exp
+
+def test_report():
+    """
+    Should return report
+    """
+    tracker = create_tracker()
+    rslt = tracker.report()
+    exp = {'inserted_contacts': 3, 'removed_members': 3, 'updated_contacts': 3,
+           'deleted_contacts': 0, 'added_members': 0}
+    assert rslt == exp
