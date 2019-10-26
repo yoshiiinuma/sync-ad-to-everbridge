429490964c69134d274f7ce7c5bb24e46b944193
=Tyler Aratani <=tyler.aratani@hawaii.gov>
2019-10-25 15:28:40 -1000

    Fixed config fire for azure function;

--- EverbridgeHTTP/EverbridgeTest/__init__.py
+++ EverbridgeHTTP/EverbridgeTest/__init__.py
@@ -10,9 +10,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     """
     Does the same function as main.py with a optional parameter to include additional groups
     """
-    runner = sync_runner.SyncRunner((pathlib.Path(__file__).parent / 'config.json', 'rb'))
-    runner.run()
+    runner = sync_runner.SyncRunner((pathlib.Path(__file__).parent / 'config.json'))
+    report = runner.run()
     return func.HttpResponse(
-            json.dumps("Sync has been completed"),
+            json.dumps(report),
             status_code=200
     )
